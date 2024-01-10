e6c203c9387d769d1da9840850b0f860ef1d8eee
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-10 11:30:54 -0300

    refactor: Normalize NavBar links for cleaner href attributes

--- Wecomp-V/src/Pages/Inicial/PaginaInicial.tsx
+++ Wecomp-V/src/Pages/Inicial/PaginaInicial.tsx
@@ -3,7 +3,7 @@ import styles from "./PaginaInicial.module.css";
 
 export default function Inicio() {
   return (
-    <div id="Início" className={styles.banner}>
+    <div id="inicio" className={styles.banner}>
       <div className={styles.bannerContainer}>
         <img className={styles.bannerImage} src={logoWecomp}></img>
         <h2 className={styles.bannerTitle}>V Semana da Computação</h2>
--- Wecomp-V/src/components/Navigation/NavBar.tsx
+++ Wecomp-V/src/components/Navigation/NavBar.tsx
@@ -1,15 +1,18 @@
 import LogoIFSP from "../Logos/IFSPLogo";
 import styles from "./Nav.module.css";
-export default function NavBar() {
-  const paths: string[] = ["Início", "Sobre", "Programaçao", "Parceiros", "Local"];
 
+export default function NavBar() {
+  const paths: string[] = ["Início", "Parceiros", "Sobre", "Programação", "Local"];
+  const removeAccents = (str: string) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  };
   return (
     <nav className={styles.navbar}>
       <LogoIFSP />
       <ul className={styles.navbarConteiner}>
         {paths.map((path, index) => (
           <li className={styles.navbarItem}>
-            <a className={styles.navbarLink} key={index} href={`#${path}`}>
+            <a className={styles.navbarLink} key={index} href={`#${removeAccents(path.toLowerCase())}`}>
               {path}
             </a>
           </li>
