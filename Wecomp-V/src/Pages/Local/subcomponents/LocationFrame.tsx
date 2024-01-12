66f6237b114a62af42ac9099fc17f3d594f00fc4
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-12 17:35:08 -0300

    feat: Implements LocationFrame component

---
+++ Wecomp-V/src/Pages/Local/subcomponents/LocationFrame.tsx
@@ -0,0 +1,16 @@
+interface LocationProps {
+  name: string;
+  address: string;
+  src: string;
+}
+export default function LocationFrame({ name, address, src }: LocationProps) {
+  return (
+    <div className="locationFrame">
+      <div>
+        <h3>{name}</h3>
+        <p>{address}</p>
+      </div>
+      <iframe src={src}></iframe>
+    </div>
+  );
+}
