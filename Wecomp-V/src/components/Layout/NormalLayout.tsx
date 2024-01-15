42f8ce2c29490e7e3b40a6ef3ba1ba1e75f676c0
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-15 14:00:13 -0300

    feat: Add background styles to page

--- Wecomp-V/src/components/Layout/NormalLayout.tsx
+++ Wecomp-V/src/components/Layout/NormalLayout.tsx
@@ -5,7 +5,7 @@ import Programacao from "../../Pages/Programacao/Programacao";
 import Sobre from "../../Pages/Sobre/Sobre";
 import Footer from "../Footer/Footer";
 import NavBar from "../Navigation/NavBar";
-/*import Background from "../ParalaxBackground/Backgroun";*/
+import Background from "../ParalaxBackground/Backgroun";
 import "../ParalaxBackground/Parallax.css";
 
 export default function NormalLayout() {
@@ -13,15 +13,16 @@ export default function NormalLayout() {
     <>
       <NavBar />
       <div className="scroll-container parallax">
-        {/*<Background isLandscape={true} />*/}
-        <div className="parallax_layer_base parallax__layer">
-          <Inicio text="V Wecomp" subtitle="Em breve" />
-          <Sobre />
-          <Parceiros isLandscape={true} />
-          <Programacao />
-          <Local isLandscape={true} />
-          <Footer isLandscape={true} />
-        </div>
+        <Background isLandscape={true}>
+          <div className="parallax_layer_base parallax__layer">
+            <Inicio text="V Wecomp" subtitle="Em breve" />
+            <Sobre />
+            <Parceiros isLandscape={true} />
+            <Programacao />
+            <Local isLandscape={true} />
+          </div>
+        </Background>
+        <Footer isLandscape={true} />
       </div>
     </>
   );
--- Wecomp-V/src/components/ParalaxBackground/Backgroun.tsx
+++ Wecomp-V/src/components/ParalaxBackground/Backgroun.tsx
@@ -1,13 +1,16 @@
+import { ReactNode } from "react";
 import "./Parallax.css";
 
 interface IBackgrounProps {
   isLandscape: boolean;
+  children: ReactNode;
 }
 
-function Background({ isLandscape }: IBackgrounProps) {
+function Background({ isLandscape, children }: IBackgrounProps) {
   return (
     <div className={`parallax_layer_back ${isLandscape ? "parallax_layer_back_landscape" : ""} parallax__layer`}>
       <div className="parallax-image" />
+      {children}
     </div>
   );
 }
