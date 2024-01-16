bbf91b6e51699365d94a0e007d605726d9ef039a
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-01-16 12:18:55 -0300

    feat: Implements types files

---
+++ src/Type/EventType.tsx
@@ -0,0 +1,14 @@
+export interface iEvent {
+  id: number;
+  name: string;
+  image: string;
+  imageDesc: string;
+  author: string;
+  date: string;
+  time: string;
+  link: string;
+  active: boolean;
+  status: string;
+  location: string;
+  description: string;
+}
